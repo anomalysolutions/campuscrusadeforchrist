86d0ea1e38ca34cad808118d29fdc221762d7b05
Amol Atigre <amol@anomaly.co.in>
2012-01-13 15:24:46 +0430

    Initial Commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Campuscrusadeforchrist::Application.config.secret_token = 'af71de8c7fb3bb7ed8af0c9eaef994a6c002c6586764c4174e0cf60be019c99dd724575588929b67444fa730b52caf70650bbf08500e82128f4da7bc1440859d'
